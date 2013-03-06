56c9eb299b9ac13426d9084a41737250c7cfbe67
Benjamin Wohlwend <piquadrat@gmail.com>
2013-03-06 21:45:42 +0100

    introduced --cache_file option to avoid unnecessary API calls

--- dyndnsimple/runner.py
+++ dyndnsimple/runner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
 from .dyndnsimple import get_ip, update_dns
 from requests.exceptions import ConnectionError
 
@@ -17,12 +18,23 @@ def run():
                         type=str, required=True)
     PARSER.add_argument('--domain', action="store", dest="domain",
                         type=str, required=True)
+    PARSER.add_argument('--cache_file', action="store", dest="cache_file",
+                        type=str, required=False)
     options = PARSER.parse_args()
 
     print "Getting IP..."
     try:
         ip_address = get_ip()
         print "OK: {}".format(ip_address)
+        if options.cache_file:
+            mode = 'r+' if os.path.exists(options.cache_file) else 'w'
+            with open(options.cache_file, mode=mode) as f:
+                old_ip = f.read().strip() if mode != 'w' else None
+                if old_ip == ip_address:
+                    print "IP did not change since last run"
+                    return
+                f.seek(0)
+                f.write(ip_address)
         if ip_address:
             print "Updating DNSimple..."
             url = "https://dnsimple.com/domains/{0}/records/{1}"\
